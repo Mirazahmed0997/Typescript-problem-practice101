1dba9746547f3f8016061ea47bb677683a2d91f4
Miraz Ahmed <ahmedmiraz87@gmail.com>
2025-07-15 04:41:00 +0600

    typeGaurd

--- Typescript-problem-practice101-main/src/Module2/TypeGaurd.ts
+++ Typescript-problem-practice101-main/src/Module2/TypeGaurd.ts
@@ -1,3 +1,16 @@
 {
-    
+    const add=(param1:string|number,param2:string|number): string|number=>
+    {
+        if(typeof param1==="number" && typeof param2==="number")
+        {
+            return param1+param2;
+        }
+        else{
+            return param1.toString()+param2.toString();
+        }
+    }
+
+    const sum1=add(5,7)
+    const sum2=add("5",7)
+    console.log(sum1,sum2)
 }
